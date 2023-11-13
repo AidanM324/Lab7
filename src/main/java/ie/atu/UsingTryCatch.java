899400964551ca5fcf487cc32f3f35205fd07306
Aidan Malone <G00423306@atu.ie>
2023-11-13 16:48:51 +0000

    Implemented a Try Catch

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
 
-    public static void main(String[] args){
-        System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Please enter a number between 0 - 9 :");
+            Scanner myscan = new Scanner(System.in);
 
             // Read user input as a Strong
             String input = myscan.nextLine();
@@ -14,12 +16,12 @@ public class UsingTryCatch {
             // Parse the input to an interger
             int myNum = Integer.parseInt(input);
 
-            if(myNum >= 0 && myNum <= 9){
+            if (myNum >= 0 && myNum <= 9) {
                 System.out.println("You entered " + myNum);
             }
-            else{
-                System.out.println("Not a valid number");
-            }
+        } catch (NumberFormatException e) {
+            System.out.println("Not a valid number");
+        }
 
     }
 }
